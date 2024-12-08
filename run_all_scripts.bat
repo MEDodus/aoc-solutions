4bc4e1ef2a258fa2f535ec09f192478fedbca41b
MDodus <mikedodus@gmail.com>
2024-12-08 16:10:58 -0500

    Commenting batch script

--- run_all_scripts.bat
+++ run_all_scripts.bat
@@ -1,6 +1,8 @@
 @echo off
 for /r python %%f in (*.py) do (
-    pushd %%~dpf
+    rem set directory to python file directory, ~dpf is the directory of the file and references f
+    pushd %%~dpf 
+    rem run python file, ~nxf is the name of the file and references f
     python "%%~nxf"
     popd
 )
